6a16a3b5d3bd2c16eaa3bbbb6b6fef38c7e7bed9
xiaowangc <42886757+xiaowangc@users.noreply.github.com>
2024-04-12 11:32:06 +0800

    Update and rename TestCodeSubbmit to TestCodeSubbmit.java

---
+++ tester/src/test/java/com/chige/tester/TestCodeSubbmit.java
@@ -0,0 +1,17 @@
+
+package com.chige.tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeSubbmit {
+
+    @Test
+    void contextLoads() {
+      int i = 0;
+      int b = 1;
+      int c = i + b;
+    }
+
+}
